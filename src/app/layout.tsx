79dc29b8deb622b72e441169fbf761431b8e4b57
Fanderson <150726052+fandersonkaecioacc@users.noreply.github.com>
2025-10-25 17:03:46 -0300

    added the google notranslate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
 import { Box, CssBaseline } from "@mui/material";
+import { Metadata } from "next";
 import Header from "./components/Header";
 import { LanguageProvider } from "./components/LanguageProvider";
 import "./global.css";
+
+export const metadata: Metadata = {
+  title: "Cardápio Multilíngue",
+  description: "Menu digital com suporte a vários idiomas",
+  other: {
+    google: "notranslate",
+  },
+};
 
 export default function RootLayout({
   children,
