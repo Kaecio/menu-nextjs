ec778bf7833cb344eeb545da98df9ee57444564e
Fanderson <150726052+fandersonkaecioacc@users.noreply.github.com>
2025-10-26 15:46:47 -0300

    refactored code

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -1,6 +1,7 @@
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Modal from "@mui/material/Modal";
+import Image from "next/image";
 
 export default function ModalComponent({
   open,
@@ -11,7 +12,7 @@ export default function ModalComponent({
 }) {
   return (
     <Box>
-      <Modal open={open} onClose={() => setOpen(false)} closeAfterTransition>
+      <Modal open={open} onClose={onClose} closeAfterTransition>
         <Box
           sx={{
             position: "fixed",
@@ -22,7 +23,7 @@ export default function ModalComponent({
           }}
         >
           <IconButton
-            onClick={() => setOpen(false)}
+            onClick={onClose}
             sx={{
               position: "absolute",
               top: 0,
@@ -31,7 +32,7 @@ export default function ModalComponent({
               zIndex: 10,
             }}
           ></IconButton>
-          <img
+          <Image
             src="/img/dinner.jpg"
             alt="teste"
             width={800}
