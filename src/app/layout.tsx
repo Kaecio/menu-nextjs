de25b4812d132983fceb10b8d10e4004b21d9e2d
Fanderson <150726052+fandersonkaecioacc@users.noreply.github.com>
2025-11-12 14:34:05 -0300

    fixed the global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Box } from "@mui/material";
 import { Metadata } from "next";
 import Header from "./components/Header";
 import { LanguageProvider } from "./components/LanguageProvider";
-// @ts-expect-error: CSS module declarations not found for side-effect import
 import "./global.css";
 
 export const metadata: Metadata = {
