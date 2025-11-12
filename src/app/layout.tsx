fada97f7707176cc8dd379304da883e97d1fa3f2
Fanderson <150726052+fandersonkaecioacc@users.noreply.github.com>
2025-11-12 14:13:18 -0300

    adjusted the flags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import { Metadata } from "next";
 import Header from "./components/Header";
 import { LanguageProvider } from "./components/LanguageProvider";
+// @ts-expect-error: CSS module declarations not found for side-effect import
 import "./global.css";
 
 export const metadata: Metadata = {
